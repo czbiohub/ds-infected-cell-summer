a38ec0c91a6b5513268e9a6e93097c0405e9a1d8
elikond <75093040+elikond@users.noreply.github.com>
2022-04-27 05:17:55 -0700

    Revert "more user choices"
    This reverts commit 9d3927827a8b9bcd5ee27a7b30df5acc68f72f21.

--- infected-cell-dash_template/infected_cell_dash/single_analysis.py
+++ infected-cell-dash_template/infected_cell_dash/single_analysis.py
@@ -3,12 +3,16 @@ import pandas as pd
 import numpy as np
 import plotly.graph_objects as go
 from collections import Counter
+<<<<<<< HEAD
 from plotly.subplots import make_subplots
 from pathlib import Path
 import name_dict
+=======
+>>>>>>> 4e0e39a5353fc3020c248428f8f9bac0718f689c
 
 
 class SingleAnalysis:
+<<<<<<< HEAD
     def __init__(self, data_path):
         self.data_path = data_path
 
@@ -36,6 +40,31 @@ class SingleAnalysis:
     
     @staticmethod
     def sig_alpha(gen_df, max_df, gene_inputs, metric, hover, virus):
+=======
+    def __init__(self, output_path):
+        self.output_path = output_path
+
+    def host_factors(self, f1):
+        dict_genes = {}
+        df1  = pd.read_csv(f1, sep = '\t')
+        df1 = df1.set_index('id')
+        df1 = df1.to_dict(orient = 'index')
+        for key in df1:
+            dict_genes[key] = -np.log(df1[key]['pos|score'])
+        k = Counter(dict_genes)
+        dict_mostcommon = dict(k.most_common(30))
+        
+        return dict_genes, dict_mostcommon
+
+    def sig_alpha(self, dict_genes, dict_mostcommon, virus):
+        sortedgenes = sorted(dict_genes.keys(), key=lambda x:x.lower())
+        sortedsiggenes = sorted(dict_mostcommon.keys(), key=lambda x:x.lower())
+
+        virus_list = list()
+        for gene in sortedgenes:
+            virus_list.append(virus)
+        
+>>>>>>> 4e0e39a5353fc3020c248428f8f9bac0718f689c
         grey_x = list()
         for i in gen_df.index:
             grey_x.append(i/len(gen_df['Genes']))
@@ -73,12 +102,19 @@ class SingleAnalysis:
         
         return red_df, grey_df, final_df
 
+<<<<<<< HEAD
     def single_plot(self, num, metric, gene_inputs, hover, virus):
         f1 = self.file(virus)
         df, df_max = self.top_hits(f1, num, metric)
         red_df, grey_df, final_df = self.sig_alpha(df, df_max, gene_inputs, metric, hover, virus)         
         fig = px.scatter(final_df, x="Genes_Alpha", y="Significance", color = 'Enriched',
                          color_discrete_sequence=["grey", "red"])
+=======
+    def single_plot(self, virus_path, virus_name):
+        dict_genes, dict_mostcommon = self.host_factors(virus_path)
+        grey_df, red_df, df = self.sig_alpha(dict_genes, dict_mostcommon, virus_path)         
+        fig = px.scatter(df, x="Genes (Alphabetically)", y="Significance", color = '30 Most Enriched Genes', color_discrete_sequence=["grey", "red"])
+>>>>>>> 4e0e39a5353fc3020c248428f8f9bac0718f689c
 
         fig.add_trace(go.Scatter(
             x= red_df['Genes_Alpha'],
@@ -106,9 +142,17 @@ class SingleAnalysis:
         ))
 
         fig.update_layout(
+<<<<<<< HEAD
             hoverlabel=dict(bgcolor="white"), title_text = str(name_dict.acronym(virus)) + ' Host Factors (CRISPR Screen)'
+=======
+            hoverlabel=dict(bgcolor="white"), title_text = str(virus_name) + ' Host Factors (CRISPR Screen)'
+>>>>>>> 4e0e39a5353fc3020c248428f8f9bac0718f689c
         )
 
         fig.update_xaxes(showticklabels=False)
 
+<<<<<<< HEAD
         return fig
+=======
+        return fig
+>>>>>>> 4e0e39a5353fc3020c248428f8f9bac0718f689c

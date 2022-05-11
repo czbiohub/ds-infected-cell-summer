b7a7d94d062e92f1a9f968008927d2266c6f9447
elikond <75093040+elikond@users.noreply.github.com>
2022-05-11 14:28:54 -0700

    built-in virus acronyms

--- infected-cell-dash_template/infected_cell_dash/name_dict.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def acronym(virus):
-    df = pd.read_csv('/Users/elianna.kondylis/Documents/GitHub/ds-infected-cell-summer/infected-cell-dash_template/infected_cell_dash/CRISPR_screen_datasets.csv')
-    df = df.set_index('Virus')
-    dict1 = df['Virus Acronym'].to_dict()
-    return dict1[virus] 
